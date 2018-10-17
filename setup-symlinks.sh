77dd11b2da32c82dd2f02e8d32a73be6f9e35e08
Luke Bennett <luke.bennett88@gmail.com>
2018-10-17 22:31:18 +1100

    Update symlink settings

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-ln -sv “~/.dotfiles/.bash_profile” ~
-ln -sv “~/.dotfiles/.eslintrc.js ~
-ln -sv “~/.dotfiles/.gitconfig” ~
-ln -sv “~/.dotfiles/.gitignore” ~
-ln -sv “~/.dotfiles/.hushlogin” ~
-ln -sv “~/.dotfiles/.hyper.js” ~
-ln -sv “~/.dotfiles/.inputrc” ~
-ln -sv “~/.dotfiles/.stylelintrc” ~
-ln -sv “~/.dotfiles/.vimrc” ~
-ln -sv “~/.dotfiles/fish/c.fish ~/.config/fish/functions/
-ln -sv “~/.dotfiles/fish/cd.fish ~/.config/fish/functions/
-ln -sv “~/.dotfiles/fish/f.fish ~/.config/fish/functions/
-ln -sv “~/.dotfiles/fish/terminate.fish ~/.config/fish/functions/
+ln -sv ~/.dotfiles/.bash_profile ~
+ln -sv ~/.dotfiles/.eslintrc.js ~
+ln -sv ~/.dotfiles/.gitattributes ~
+ln -sv ~/.dotfiles/.gitconfig ~
+ln -sv ~/.dotfiles/.gitignore ~
+ln -sv ~/.dotfiles/.hushlogin ~
+ln -sv ~/.dotfiles/.hyper.js ~
+ln -sv ~/.dotfiles/.inputrc ~
+ln -sv ~/.dotfiles/.stylelintrc ~
+ln -sv ~/.dotfiles/.vimrc ~
+ln -sv ~/.dotfiles/config.fish ~/.config/fish/
+ln -sv ~/.dotfiles/fishfile ~/.config/fish/
