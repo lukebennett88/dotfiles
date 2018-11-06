39808b9f812b148a45edfd33449fb28c62342f15
Luke Bennett <luke.bennett88@gmail.com>
2018-11-06 19:23:47 +1100

    Update Fisher bootstrap installation

--- config.fish
+++ config.fish
@@ -1,9 +1,8 @@
 # Install Fisher
 if not functions -q fisher
-    echo "Installing fisher for the first time..." >&2
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fisher
+    fish -c fisher
 end
 
 # Shorten `clear` command to `c`
