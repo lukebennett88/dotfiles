2d4a4a5c05d22ddb39ee7aa1192d98728cf09ac7
Luke Bennett <luke.bennett88@gmail.com>
2019-01-09 21:17:01 +1100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,20 @@
 module.exports = {
   env: {
     browser: true,
-    node: true,
-    commonjs: true,
-    es6: true,
-    worker: true,
-    jest: true,
-    jquery: true,
-    mongo: true,
-    applescript: true,
-    serviceworker: true
+    es6: true
   },
-  extends: [
-    "plugin:jsx-a11y/recommended",
-    "plugin:prettier/recommended"
-  ],
-  parser:
-    "babel-eslint",
+  extends: ["plugin:jsx-a11y/recommended", "plugin:prettier/recommended"],
+  plugins: ["react"],
+  globals: {
+    graphql: false
+  },
+  parser: "babel-eslint",
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType:
-      "module",
+    sourceType: "module",
     ecmaFeatures: {
-      jsx: true,
-      impliedStrict: true
+      experimentalObjectRestSpread: true,
+      jsx: true
     }
   },
-  plugins: [
-    "jsx-a11y",
-    "prettier",
-    "react"
-  ],
-  rules: {
-    "no-console": 1
-  }
+  plugins: ["jsx-a11y", "prettier", "react"]
 };
