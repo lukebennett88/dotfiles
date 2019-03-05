829c9f0d3a4544ef3b672b0a141e1aa402cb60b9
Luke Bennett <luke.bennett88@gmail.com>
2019-03-05 13:24:54 +1100

    Use Wes Bos ESLint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,5 @@
-module.exports = {
-  env: {
-    applescript: true,
-    browser: true,
-    commonjs: true,
-    es6: true,
-    jest: true,
-    jquery: true,
-    mongo: true,
-    node: true,
-    serviceworker: true,
-    worker: true,
-  },
-  extends: ['plugin:jsx-a11y/recommended', 'plugin:prettier/recommended'],
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-      impliedStrict: true,
-    },
-  },
-  globals: {
-    graphql: false,
-  },
-  plugins: ['jsx-a11y', 'prettier', 'react'],
-};
+{
+  "extends": [
+    "wesbos"
+  ]
+}
