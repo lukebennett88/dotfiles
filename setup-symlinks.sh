48dbcd36b18ea7e8415b85bef3a993d3868d19f6
Luke Bennett <hello@lukebennett.com.au>
2019-06-19 20:15:44 +1000

    Set up some fish stuff

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -10,3 +10,13 @@ ln -s -f $HOME/.dotfiles/.eslintrc $HOME/.eslintrc
 
 # Fish
 ln -s -f $HOME/.dotfiles/fish $HOME/.config/
+
+# Set up Fisher
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
+# Set up node
+nvm use lts
